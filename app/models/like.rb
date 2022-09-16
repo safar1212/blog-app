1faa87c5473c76ef37a3e1fdcdf7bf75f90e7864
safar ali <miqra019@gmail.com>
2022-09-16 13:41:33 +0500

    solve linters errors

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,5 +1,5 @@
 class Like < ActiveRecord::Base
-    belongs_to :user, class_name: 'User', foreign_key: 'AuthorId'
+  belongs_to :user, class_name: 'User', foreign_key: 'AuthorId'
   belongs_to :post, class_name: 'Post', foreign_key: 'PostId'
 
   after_save :update_counter
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
-    has_many :posts, class_name: 'Post', foreign_key: 'AuthorId'
-   has_many :comments, class_name: 'Comment', foreign_key: 'AuthorId'
-   has_many :likes, class_name: 'Like', foreign_key: 'AuthorId'
+  has_many :posts, class_name: 'Post', foreign_key: 'AuthorId'
+  has_many :comments, class_name: 'Comment', foreign_key: 'AuthorId'
+  has_many :likes, class_name: 'Like', foreign_key: 'AuthorId'
 
   def recent_post
     posts.order(create_at: :desc).limit(3)
