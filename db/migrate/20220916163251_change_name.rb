99af7f23264b23e4a442e88a9722bbaf5539d4c1
safar ali <miqra019@gmail.com>
2022-09-23 19:22:09 +0500

    Solve all the problems requested my last reviewer

--- db/migrate/20220916163251_change_name.rb
+++
@@ -1,4 +0,0 @@
-class ChangeName < ActiveRecord::Migration[7.0]
-  def change
-  end
-end
--- db/migrate/20220923084840_remove_post_id_from_post.rb
+++
@@ -1,5 +0,0 @@
-class RemovePostIdFromPost < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :posts, :PostId, :integer
-  end
-end
