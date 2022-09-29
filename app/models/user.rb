5200c8967341141e57a81236416a25eabb4801bf
safar ali <miqra019@gmail.com>
2022-09-29 22:32:24 +0500

    Create log in form

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,12 +10,21 @@ class User < ActiveRecord::Base
   has_many :posts, foreign_key: 'user_id'
   has_many :comments, foreign_key: 'user_id'
 
+  before_validation :default_values
+
   def three_recent_posts
     posts.limit(3).order(created_at: :desc)
   end
 
   validates :Name, presence: true
   validates :PostsCounter, numericality: { greater_than_or_equal_to: 0 }
+
+  private
+
+  def default_values
+    self.posts_counter = 0
+    self.photo = 'https://img.myloview.com/posters/social-media-user-icon-default-avatar-profile-image-400-251200036.jpg'
+  end
 
  
 end
